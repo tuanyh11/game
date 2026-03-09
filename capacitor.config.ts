815e4fa7c09623af24484766ec3006df1241b21b
leo <you@example.com>
2026-03-09 20:34:36 +0700

    Build: Setup Capacitor for Mobile (Android & iOS)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuanyh.game',
+  appName: 'Pixel Empires',
+  webDir: 'dist'
+};
+
+export default config;
